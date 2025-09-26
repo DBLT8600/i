0b133e25416da6f9877557c9b10aad58750d86b7
arch <arch@workbench>
2025-09-26 09:53:31 +0900

    update arch-arm

--- arch-arm.sh
+++ arch-arm.sh
@@ -2,65 +2,41 @@
 
 set -eo pipefail
 
+if (( DEBUG )); then
+    set -x
+fi
+
 if (( UID )); then
     echo "sudo $0" >&2; exit 1
 fi
 
-is_aarch64=0
-if [[ "$1" == '-64' || "$1" == '--64' ]]; then
-    is_aarch64=1; shift
-fi
-
-dev="$1"
-boot="${dev}p1"
-root="${dev}p2"
-
-is_usbboot=0
-if [[ "$dev" =~ /dev/sd* ]]; then
-    boot="${dev}1" root="${dev}2" is_usbboot=1; shift
-fi
-
-echo "wipefs"
-
-wipefs -a "$dev"
-
-echo "parted"
-
-parted -s "$dev" -- mklabel msdos mkpart primary fat32 1MiB 513MiB mkpart primary ext4 513MiB 100%
-
 tmp=$(mktemp -d "$PWD/tmp-XXXXXXXX")
-bootdir="$tmp/boot"
-rootdir="$tmp/root"
-install -Ddm755 "$bootdir" "$rootdir"
 
-echo "mkfs.vfat $boot"
+trap 'trap - ERR EXIT; umount -R "$tmp"; rm -rf "$tmp"' ERR EXIT INT
 
-mkfs.vfat "$boot"
-mount "$boot" "$bootdir"
+boot_dev=${1}p1
+root_dev=${1}p2
 
-echo "mkfs.ext4 $root"
+boot_size=1024
+boot_start=1
+boot_end=$(( boot_start + boot_size ))
 
-mkfs.ext4 -F "$root"
-mount "$root" "$rootdir"
+root_start=$boot_end
 
-trap 'trap - ERR EXIT; umount "$bootdir" "$rootdir"; rm -rf "$tmp"' ERR EXIT INT
+parted_args=(
+    mklabel msdos
+    mkpart boot fat32 ${boot_start}MiB ${boot_end}MiB
+    mkpart root ext4 ${root_start}MiB 100%
+)
 
 url=http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-armv7-latest.tar.gz
-if (( $is_aarch64 )); then
-    url=http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
-fi
 
-echo "curl $url"
-
-curl -fsSL "$url" | bsdtar -xf - -C "$rootdir"
-sync
-
-mv "$rootdir/boot"/* "$bootdir"
-
-if (( $is_usbboot )); then
-    sed "s,/dev/mmcblk0p,/dev/sda," -i "$rootdir/etc/fstab"
-
-    if [[ -f "$bootdir/cmdline.txt" ]]; then
-        sed "s,/dev/mmcblk0p,/dev/sda," -i "$bootdir/cmdline.txt"
-    fi
-fi
+parted -s "$1" -- "${parted_args[@]}" \
+    mkfs.ext4 -F "$root_dev" \
+    mount "$root_dev" "$tmp" \
+    mkfs.vfat "$boot_dev" \
+    mkdir "$tmp/boot" \
+    mount "$boot_dev" "$tmp/boot" \
+    curl -fsSL "$url" | bsdtar -xf - -C "$tmp" \
+    sync \
+    echo finish
